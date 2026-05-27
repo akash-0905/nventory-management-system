c395cbf044d2f37ed3b0fcb7c5dc3d3785ac57d7
akash-0905 <vsakash057@gmail.com>
2026-05-27 20:27:35 +0530

    Created Controller to handle restAPI's

--- src/main/java/com/inventory/inventory_management/controller/ProductController.java
+++ src/main/java/com/inventory/inventory_management/controller/ProductController.java
@@ -1,4 +1,22 @@
 package com.inventory.inventory_management.controller;
 
+import com.inventory.inventory_management.Repository.ProductRepository;
+import com.inventory.inventory_management.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping
+    public Product saveProduct(@RequestBody Product product){
+        return productRepository.save(product);
+    }
 }
