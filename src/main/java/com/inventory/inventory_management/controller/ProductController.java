224e88c3b14436664de3f67f6031e7c57569db7a
akash-0905 <vsakash057@gmail.com>
2026-05-28 19:48:15 +0530

    Getting  by FindById

--- src/main/java/com/inventory/inventory_management/controller/ProductController.java
+++ src/main/java/com/inventory/inventory_management/controller/ProductController.java
@@ -3,10 +3,9 @@ package com.inventory.inventory_management.controller;
 import com.inventory.inventory_management.Repository.ProductRepository;
 import com.inventory.inventory_management.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -19,4 +18,17 @@ public class ProductController {
     public Product saveProduct(@RequestBody Product product){
         return productRepository.save(product);
     }
+
+    @GetMapping
+    public List<Product> getAllProduct(){
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductByID(@PathVariable Long id){
+        return productRepository.findById(id).orElse(null);
+    }
+
+
+
 }
