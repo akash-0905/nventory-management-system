3848f684be8aee8b2989bacc99bb0658a5aa3003
akash-0905 <vsakash057@gmail.com>
2026-06-01 11:00:31 +0530

    Added Custom Status Code and Body using ResponseEntity

--- src/main/java/com/inventory/inventory_management/controller/ProductController.java
+++ src/main/java/com/inventory/inventory_management/controller/ProductController.java
@@ -3,7 +3,10 @@ package com.inventory.inventory_management.controller;
 import com.inventory.inventory_management.entity.Product;
 import com.inventory.inventory_management.service.ProductService;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +19,10 @@ public class ProductController {
     private ProductService productService;
 
     @PostMapping
-    public Product saveProduct(@Valid @RequestBody Product product){
-        return productService.saveProduct(product);
+    public ResponseEntity<Product> saveProduct(@Valid @RequestBody Product product){
+
+        Product savedProduct = productService.saveProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
     }
 
     @GetMapping
