e35ec7325609d79ab5926fa551780e802fa8e8d3
akash-0905 <vsakash057@gmail.com>
2026-06-03 11:38:23 +0530

    Created response for DTO

--- src/main/java/com/inventory/inventory_management/dto/ProductResponseDTO.java
+++ src/main/java/com/inventory/inventory_management/dto/ProductResponseDTO.java
@@ -2,5 +2,31 @@ package com.inventory.inventory_management.dto;
 
 public class ProductResponseDTO {
 
+    private Long id;
+    private String name;
+    private double price;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
